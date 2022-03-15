782ebae2fc1a14721363eac9ffdb60c9ab0cabf9
Julio Carvalho <jcarvalhok3@gmail.com>
2022-03-15 15:35:15 -0300

    swagger

--- src/main/java/br/com/crud/CrudUsuarioLoginApplication.java
+++ src/main/java/br/com/crud/CrudUsuarioLoginApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class CrudUsuarioLoginApplication {
 
